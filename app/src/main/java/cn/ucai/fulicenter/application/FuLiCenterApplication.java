b5f8de9342301219986013ebb21bf191f8f76f17
juheng <1624371584@qq.com>
2017-01-10 16:03:33 +0800

    添加闪屏和跳转动画

---
+++ app/src/main/java/cn/ucai/fulicenter/application/FuLiCenterApplication.java
@@ -0,0 +1,9 @@
+package cn.ucai.fulicenter.application;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/1/10 0010.
+ */
+public class FuLiCenterApplication extends Application {
+}
