33fb8a120036a873a3e2e48c278bedd530128a0d
juheng <1624371584@qq.com>
2017-01-11 19:29:47 +0800

    精选首页数据显示、Fragment切换

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/MainActivity.java
@@ -1,66 +1,104 @@
 package cn.ucai.fulicenter.controller.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.controller.fragment.BoutiqueFragment;
+import cn.ucai.fulicenter.controller.fragment.NewGoodsFragment;
 
 public class MainActivity extends AppCompatActivity {
     int index, currentIndex;
-    RadioButton rbNewGoods, rbBoutique, rbCategory, rbCart, rbPersonal;
     RadioButton[] rbs = new RadioButton[5];
+    Fragment[] mFragements;
+
+    @BindView(R.id.layout_new_good)
+    RadioButton layoutNewGood;
+    @BindView(R.id.layout_boutique)
+    RadioButton layoutBoutique;
+    @BindView(R.id.layout_category)
+    RadioButton layoutCategory;
+    @BindView(R.id.layout_cart)
+    RadioButton layoutCart;
+    @BindView(R.id.layout_personal_center)
+    RadioButton layoutPersonalCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rbNewGoods = (RadioButton) findViewById(R.id.layout_new_good);
-        rbBoutique = (RadioButton) findViewById(R.id.layout_boutique);
-        rbCategory = (RadioButton) findViewById(R.id.layout_category);
-        rbCart = (RadioButton) findViewById(R.id.layout_cart);
-        rbPersonal = (RadioButton) findViewById(R.id.layout_personal_center);
+        ButterKnife.bind(this);
 
-        rbs[0] = rbNewGoods;
-        rbs[1] = rbBoutique;
-        rbs[2] = rbCategory;
-        rbs[3] = rbCart;
-        rbs[4] = rbPersonal;
+        rbs[0] = layoutNewGood;
+        rbs[1] = layoutBoutique;
+        rbs[2] = layoutCategory;
+        rbs[3] = layoutCart;
+        rbs[4] = layoutPersonalCenter;
+
+        initFragment();
     }
+
+    private void initFragment() {
+        mFragements=new Fragment[5];
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction ft=manager.beginTransaction();
+        mFragements[0]=new NewGoodsFragment();
+        mFragements[1]=new BoutiqueFragment();
+        ft.add(R.id.layout_menu,mFragements[0]).commit();
+    }
+
 
     public void onCheckedChange(View view) {
         switch (view.getId()) {
             case R.id.layout_new_good:
-                index=0;
+                index = 0;
                 break;
             case R.id.layout_boutique:
-                index=1;
+                index = 1;
                 break;
             case R.id.layout_category:
-                index=2;
+                index = 2;
                 break;
             case R.id.layout_cart:
-                index=3;
+                index = 3;
                 break;
             case R.id.layout_personal_center:
-                index=4;
+                index = 4;
                 break;
         }
-        if(currentIndex!=index){
+        if (currentIndex != index) {
+            setFragment();
             setRadioStatus();
         }
 
     }
-    private void setRadioStatus(){
-        for (int i=0;i<rbs.length;i++){
-            if(index!=i){
+
+    private void setFragment() {
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction ft=manager.beginTransaction();
+        if (!mFragements[index].isAdded()) {
+            ft.add(R.id.layout_menu, mFragements[index]);
+        }
+        ft.show(mFragements[index])
+                .hide(mFragements[currentIndex]);
+        ft.commit();
+    }
+
+    private void setRadioStatus() {
+        for (int i = 0; i < rbs.length; i++) {
+            if (index != i) {
                 rbs[i].setChecked(false);
-            }else{
+            } else {
                 rbs[i].setChecked(true);
             }
-            currentIndex=index;
+            currentIndex = index;
         }
     }
 }
---
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/BoutiqueFragment.java
@@ -0,0 +1,161 @@
+package cn.ucai.fulicenter.controller.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.controller.activity.MainActivity;
+import cn.ucai.fulicenter.controller.adapter.BoutiqueAdapter;
+import cn.ucai.fulicenter.model.bean.BoutiqueBean;
+import cn.ucai.fulicenter.model.net.IModelBoutique;
+import cn.ucai.fulicenter.model.net.ModelBoutique;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.ConvertUtils;
+import cn.ucai.fulicenter.view.SpaceItemDecoration;
+
+import static cn.ucai.fulicenter.application.I.ACTION_PULL_UP;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BoutiqueFragment extends Fragment {
+
+
+    @BindView(R.id.tv_refresh)
+    TextView tvRefresh;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.srl)
+
+    SwipeRefreshLayout srl;
+
+    BoutiqueAdapter mAdapter;
+    ArrayList<BoutiqueBean> mList;
+    LinearLayoutManager mManager;
+
+    IModelBoutique mModel;
+
+    MainActivity mContext;
+
+    public BoutiqueFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_boutique, container, false);
+        ButterKnife.bind(this, view);
+        mModel = new ModelBoutique();
+        mList = new ArrayList<>();
+        mContext = (MainActivity) getContext();
+        mAdapter = new BoutiqueAdapter(mContext, mList);
+        initView();
+        initData();
+        setListener();
+        return view;
+    }
+
+    private void setListener() {
+        setPullDownListener();
+        //setPullUpListener();
+    }
+
+   /* private void setPullUpListener() {
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int lastPosition=mManager.findLastVisibleItemPosition();
+                if(mAdapter.isMore()&&newState==RecyclerView.SCROLL_STATE_IDLE&&mAdapter.getItemCount()-1==lastPosition){
+                    download(ACTION_PULL_UP);
+                }
+            }
+        });
+    }*/
+
+    private void setPullDownListener() {
+        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                srl.setRefreshing(true);
+                tvRefresh.setVisibility(View.VISIBLE);
+                download(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+
+    private void initData() {
+        download(I.ACTION_DOWNLOAD);
+
+    }
+
+    private void download(final int action) {
+        mModel.downData(mContext, new OnCompleteListener<BoutiqueBean[]>() {
+            @Override
+            public void onSuccess(Object result) {
+                BoutiqueBean[] mResult= (BoutiqueBean[]) result;
+                ArrayList<BoutiqueBean> list = ConvertUtils.array2List(mResult);
+                /*mAdapter.setMore(mResult != null&&mResult.length>0);
+                if (!mAdapter.isMore()) {
+                    if(action==I.ACTION_PULL_UP){
+                        mAdapter.setFooter("没有更多数据");
+                    }
+
+                    return;
+                }
+                mAdapter.setFooter("加载更多数据");*/
+                mAdapter.setFooter("没有更多数据");
+                switch (action){
+                    case I.ACTION_DOWNLOAD:
+                        mAdapter.initData(list);
+                        break;
+                    case I.ACTION_PULL_DOWN:
+                        srl.setRefreshing(false);
+                        tvRefresh.setVisibility(View.GONE);
+                        mAdapter.initData(list);
+                        break;
+                    case I.ACTION_PULL_UP:
+                        mAdapter.addData(list);
+                        break;
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                Toast.makeText(mContext, error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initView() {
+        srl.setColorSchemeColors(getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow));
+
+        mManager = new LinearLayoutManager(mContext);
+        recyclerView.setLayoutManager(mManager);
+
+        recyclerView.addItemDecoration(new SpaceItemDecoration(25));
+
+        recyclerView.setAdapter(mAdapter);
+    }
+
+}
