59b36bc42b26c088571a0348484eadab63e87ca6
juheng <1624371584@qq.com>
2017-01-16 13:12:51 +0800

    分类二级页面排序箭头的修改及自定义Button

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/CategoryDetailActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/CategoryDetailActivity.java
@@ -1,10 +1,12 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +18,7 @@ import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.controller.fragment.NewGoodsFragment;
 import cn.ucai.fulicenter.model.net.IModelNewGoods;
 import cn.ucai.fulicenter.model.net.ModelNewGoods;
+import cn.ucai.fulicenter.view.CatFilterButton;
 
 public class CategoryDetailActivity extends AppCompatActivity {
 
@@ -23,12 +26,19 @@ public class CategoryDetailActivity extends AppCompatActivity {
     IModelNewGoods model;
     @BindView(R.id.iv_category_detail_back)
     ImageView ivCategoryDetailBack;
-    @BindView(R.id.tv_category_detail_textView)
-    TextView tvCategoryDetailTextView;
-    @BindView(R.id.iv_category_detail_image)
-    ImageView ivCategoryDetailImage;
 
     NewGoodsFragment mNewGoodsFragment;
+
+    boolean flag1 = true;
+    boolean flag2 = true;
+    @BindView(R.id.category_detail_addTime)
+    Button categoryDetailAddTime;
+    @BindView(R.id.category_detail_price)
+    Button categoryDetailPrice;
+   @BindView(R.id.catButton_category)
+    CatFilterButton catButtonCategory;
+
+    String groupName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +53,7 @@ public class CategoryDetailActivity extends AppCompatActivity {
     private void initFragment() {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction ft = manager.beginTransaction();
-        mNewGoodsFragment=new NewGoodsFragment();
+        mNewGoodsFragment = new NewGoodsFragment();
         ft.add(R.id.category_fragment_menu, mNewGoodsFragment)
                 .show(mNewGoodsFragment)
                 .commit();
@@ -51,27 +61,57 @@ public class CategoryDetailActivity extends AppCompatActivity {
 
 
     private void initData() {
-        String name = getIntent().getStringExtra("CategoryChildBean_name");
-        tvCategoryDetailTextView.setText(name);
+        groupName = getIntent().getStringExtra("CategoryChildBean_name");
+        catButtonCategory.setText(groupName);
     }
 
-    @OnClick(R.id.iv_category_detail_back)
-    public void onClick() {
-        this.finish();
+
+
+    /*private void showImage1(boolean flag) {
+        ivUp.setImageResource(flag ? R.drawable.arrow_order_down : R.drawable.arrow_order_up);
+
     }
 
-    @OnClick({R.id.iv_category_detail_back, R.id.category_detail_price, R.id.category_detail_addTime})
+    private void showImage2(boolean flag) {
+        ivDown.setImageResource(flag ? R.drawable.arrow_order_down : R.drawable.arrow_order_up);
+    }*/
+
+
+    @OnClick({R.id.iv_category_detail_back,R.id.catButton_category,R.id.category_detail_addTime, R.id.category_detail_price})
     public void onClick(View view) {
+        Drawable right;
         switch (view.getId()) {
             case R.id.iv_category_detail_back:
                 this.finish();
                 break;
-            case R.id.category_detail_price:
-                mNewGoodsFragment.sortGoods(I.SORT_BY_PRICE_ASC);
+            case R.id.catButton_category:
+                catButtonCategory.initCatFilterButton(groupName,null);
                 break;
             case R.id.category_detail_addTime:
-                mNewGoodsFragment.sortGoods(I.SORT_BY_ADDTIME_ASC);
+                if (flag1) {
+                    mNewGoodsFragment.sortGoods(I.SORT_BY_ADDTIME_ASC);
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
+                } else {
+                    mNewGoodsFragment.sortGoods(I.SORT_BY_ADDTIME_DESC);
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
+                }
+                right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+                categoryDetailAddTime.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, right, null);
+                flag1 = !flag1;
+                break;
+            case R.id.category_detail_price:
+                if (flag2) {
+                    mNewGoodsFragment.sortGoods(I.SORT_BY_PRICE_ASC);
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
+                } else {
+                    mNewGoodsFragment.sortGoods(I.SORT_BY_PRICE_DESC);
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
+                }
+                right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+                categoryDetailPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, right, null);
+                flag2 = !flag2;
                 break;
         }
     }
+
 }
